bd7b61a49f35bae788f379b5f993c3ad727f1c7a
jf <jan.friedli@immerda.ch>
2022-05-06 19:02:27 +0200

    wip

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # Dev Setup
 
 run it with docker and docker-compose installed: `composer run-script serve`
+
+and to live reload the style run `npm run watch`
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     './content/**/*.md'
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'red': '#f0412d',
+      'purple': {
+        100: '#5A46E6',
+        200: '#292546',
+      },
+      'black': '#000',
+    },
+    extend: {
+
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
